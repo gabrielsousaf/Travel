1921b037e6fd0cf51753e87dd491bb32d92f0fa5
gabrielsousaf <gs294860@gmail.com>
2024-01-30 14:13:03 -0300

    Config Constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,17 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        Home
+        <Image src="/hilink-logo.svg" alt="logo"
+        width={74}
+        height={29}
+        />
+        <ul className="hidden h-full gap-12 lg:flex">
+          {['Home']}
+        </ul>
       </Link>
     </nav>
   )
