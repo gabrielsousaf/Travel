51bf029f5bdd04dbd2e37ec63b50885ea5f49c78
gabrielsousaf <gs294860@gmail.com>
2024-01-31 15:03:55 -0300

    Update components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
+"use client"
+ 
+import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
+import Button from "./Button"
+import { useState } from "react"
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -9,12 +20,44 @@ const Navbar = () => {
         width={74}
         height={29}
         />
-        <ul className="hidden h-full gap-12 lg:flex">
-          {['Home']}
-        </ul>
       </Link>
+
+      {(isOpen && window.matchMedia("(max-width: 768px)").matches) && (
+        <ul className="h-full gap-12 lg:flex md:flex">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      )}
+
+
+      <div className="lg:flexCenter hidden">
+        <Button 
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleMenu}
+      />
     </nav>
   )
 }
 
 export default Navbar
+
+
