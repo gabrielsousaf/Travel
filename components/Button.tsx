f1f4145d2a1e34a96648e60c0bcb85bf9dcff780
gabrielsousaf <gs294860@gmail.com>
2024-02-02 13:45:51 -0300

    Update Footer

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
 
 const Button = ({ type, title, icon, variant, full} : ButtonProps) => {
   return (
-    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
+    <button type={type} className={`flexCenter transition-all duration-300 ease hover:scale-110 gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
