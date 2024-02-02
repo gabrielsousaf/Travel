434a20fe9bc9e847203666e3b6deb04f32c0bb58
gabrielsousaf <gs294860@gmail.com>
2024-02-02 11:00:47 -0300

    Update Guide

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -8,29 +8,30 @@ interface CampProps {
   peopleJoined: string;
 }
 
-const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) => {
-  return(
-    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl:rounded-5xl 2xl:rounded-5xl`}>
-      <div className="flex h-full flex-xol items-start justify-between p-6 lg:px-20 lg:py-10 border-2 border-blue-500">
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
+  return (
+    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
-        <div className="rounded-full bg-green-50 p4">
-          <Image
-            src="/folded-map.svg"
-            alt="map"
-            width={28}
-            height={28}
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <h4 className="bold-18 text-white">{title}</h4>
-          <p className="regular-14 text-white">{subtitle}</p>
+          <div className="rounded-full bg-green-50 p-4">
+            <Image
+              src="/folded-map.svg"
+              alt="map"
+              width={28}
+              height={28}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
         </div>
 
         <div className="flexCenter gap-6">
           <span className="flex -space-x-4 overflow-hidden">
             {PEOPLE_URL.map((url) => (
               <Image
-              className="inline-block h-10 w-10 rounded-full"
+                className="inline-block h-10 w-10 rounded-full"
                 src={url}
                 key={url}
                 alt="person"
@@ -44,7 +45,6 @@ const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) =
           </p>
         </div>
 
-        </div>
       </div>
     </div>
   )
@@ -52,8 +52,8 @@ const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) =
 
 const Camp = () => {
   return (
-    <section className='2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'> 
-      <div className="hide-scrollbar flecx h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+    <section className='2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
           backgroundImage="bg-bg-img-1"
           title="Putuk Truno Camp"
@@ -74,14 +74,14 @@ const Camp = () => {
             <strong>Feeling Lost</strong> And Not Knowing The Way?
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">
-          Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+            Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
           </p>
           <Image
-            src="quote.svg"   
+            src="quote.svg"
             alt="camp-2"
             width={186}
             height={219}
-            className="camp-quote"      
+            className="camp-quote"
           />
 
         </div>
--- components/Features.tsx
+++ components/Features.tsx
@@ -5,7 +5,7 @@ import { FEATURES } from '@/constants'
 const Features = () => {
   return (
     <section className='flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
-      <div className="max-container padding-container relative w-full justify-end">
+      <div className="max-container padding-container relative w-full flex justify-end">
         <div className='flex flex-1 lg:min-h-[900px]'>
           <Image
             src="/phone.png"
